26fc99e5386e75adfaff0030b883882f20611d70
FedeZ <maniacfede@gmail.com>
2017-10-12 11:27:02 -0300

    Se creo el sistema de registro.

--- clashofplanets/game/views.py
+++ clashofplanets/game/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import login, authenticate
 
-# Create your views here.
+def signupView(request): # Sign Up View (Allow Users to register on system)
+    if request.method == 'POST':
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            raw_password = form.cleaned_data.get('password1')
+            user = authenticate(username=username, password=raw_password)
+            login(request, user)
+            return redirect('home')
+    else:
+        form = SignUpForm()
+    return render(request, 'register.html', {'form': form})
