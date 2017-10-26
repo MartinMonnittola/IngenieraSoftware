457c6a41c1d3c75dbfdf4f6cadc5e13c7e1486b4
nightcurious <nightcurious@gmail.com>
2017-10-26 10:27:15 -0300

    Modificacion en start.sh para que envie kill a los procesos de django

--- clashofplanets/start.sh
+++ clashofplanets/start.sh
@@ -1,13 +1,32 @@
 #!/usr/bin/bash
 
 
+HOST=127.0.0.1:8000
 UPDATE_TIME=2
+COUNT=0
 
-python manage.py runserver &
+# When receive a ctrl+c
+function ctrl_c() {
+    while [ $COUNT -lt 3 ]; do
+        echo -e "Waiting until django processes spawn..."
+        sleep 2
+        COUNT=$(ps ax | grep $HOST | wc -l)
+    done
+
+    echo -e Killing django...
+    fuser -k 8000/tcp
+    exit 0
+}
+
+# Catch ctrl+c
+trap ctrl_c INT
+
+/usr/bin/python manage.py runserver $HOST &
 
 # Wait 5 seconds until the game loads
 sleep 5
 
+# Sleep and run the updates
 while true
 do
     sleep $UPDATE_TIME
