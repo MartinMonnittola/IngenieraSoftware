9b7e7b136a19e64510e0ef8c2fe215815288eff4
Leonardo <leon.mass.music@gmail.com>
2017-11-15 02:05:51 -0300

    Separar comandos generacion-missiles

--- clashofplanets/game/management/commands/generate_resources.py
+++ clashofplanets/game/management/commands/generate_resources.py
@@ -9,15 +9,12 @@ class Command(BaseCommand):
         for g in active_games:
             planets_in_active_rooms = Planet.objects.filter(game=g)
             for p in planets_in_active_rooms:
-                if p.population_qty < 10000:
-                    cantidad_asig = p.population_qty * p.population_distr / 100
-                    calculo_generar_pop = cantidad_asig / g.const_population
-                    p.population_qty += calculo_generar_pop
-                    if p.population_qty + calculo_generar_pop > 10000:
-                        p.population_qty = 10000
-                    else:
-                        p.population_qty += calculo_generar_pop
-                if p.shield_perc < 100:
+                ##### DISTRIBUTION #####
+                cantidad_asig = p.population_qty * p.population_distr / 100
+                calculo_generar_pop = cantidad_asig / g.const_population
+                p.population_qty += calculo_generar_pop
+
+                if (p.shield_perc < 100):
                     cant_asig_shield = p.population_qty * p.shield_distr / 100
                     calculo_generar_shield = cant_asig_shield / g.const_shield
                     if (p.shield_perc + calculo_generar_shield) > 100:
@@ -28,11 +25,4 @@ class Command(BaseCommand):
                 calculo_generar_missile = cant_asig_mis / g.const_missile
                 p.missiles_qty += calculo_generar_missile
                 p.save()
-                ##### MISSILES ####
-                missiles_from_planet = Missile.objects.filter(owner=p,is_active=True)
-                for m in missiles_from_planet:
-                    m_2_t = m.time_to_target()
-                    if m_2_t.total_seconds() <= 0:
-                        m.deal_damage()
-
-        return "Planets have been refilled"
+            return "Planets have been refilled"
