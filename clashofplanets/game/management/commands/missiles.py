fa69abde5741172f23e800b7da060f5626dad6e5
Leonardo <leon.mass.music@gmail.com>
2017-11-20 15:50:37 -0300

    Modificado filtrado salas activas

--- clashofplanets/game/management/commands/missiles.py
+++ clashofplanets/game/management/commands/missiles.py
@@ -5,15 +5,14 @@ class Command(BaseCommand):
     help = 'Check and deal damage of active missiles.'
 
     def handle(self, *args, **options):
-        active_games = Game.objects.filter(game_started=True)
+        active_games = Game.objects.filter(game_started=True, game_finished=False)
         for g in active_games:
             planets_in_active_rooms = Planet.objects.filter(game=g)
             for p in planets_in_active_rooms:
                 ##### MISSILES #####
                 missiles_from_planet = Missile.objects.filter(owner=p,is_active=True)
                 for m in missiles_from_planet:
-                    print "hols"
                     m_2_t = m.time_to_target()
                     if m_2_t.total_seconds() <= 0:
                         m.deal_damage()
-        return "Missiles have been checked"
+        return "Command Missile Check Executed"
