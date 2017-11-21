4c4d6d1233210fb276345f9c28405473e141e1e2
martinmonnittola <martinmonnittola@gmail.com>
2017-11-21 19:40:28 -0300

    Se ha agregado los test para el campo bot_def_num y bot_ofc_num, en el modelo GameForm

--- clashofplanets/game/tests/testsForm.py
+++ clashofplanets/game/tests/testsForm.py
@@ -1,27 +1,36 @@
 from django.test import TestCase
-from game.forms import * 
+from .forms import * 
 
 
 class GameFormTestsCase(TestCase):
-    def setUp(self):
-        user = User(username="GGG", first_name="GGGLJH", last_name= "GGGLKJLVG",
-                    email="dvdfv@gmail.com", password="Aa12345678")
-        user.save()
+
+    def test_form_field_bot_def_num_do_not_accept_values_negatives(self):
+        form_data = {"planet_name":"fgh", "game_name": "ggdf", "game_mode":1, "max_players":20, 
+                     "num_alliances": 3, "bot_def_num": -1,"bot_ofc_num": 2}
+        form = gameForm(form_data)
+        self.assertFalse(form.is_valid())
+
+    def test_form_field_bot_ofc_num_do_not_accept_values_negatives(self):
+        form_data = {"planet_name":"fgh", "game_name": "ggdf", "game_mode":1, "max_players":20, 
+                     "num_alliances": 3, "bot_def_num": 1,"bot_ofc_num": -2}
+        form = gameForm(form_data)
+        self.assertFalse(form.is_valid())
+
 
     def test_form_field_max_player_does_not_accept_value_negative_or_zero(self):
-        user = User.objects.get(pk=1)
-        form_data = {"game_name": "ghnfcg", "max_players":-20, "planet_name":"dbfgbdfgb"}
+        form_data =  {"planet_name":"fgh", "game_name": "ggdf", "game_mode":1, "max_players":-20, 
+                     "num_alliances": 3, "bot_def_num": 1,"bot_ofc_num": 2}
         form = gameForm(form_data)
         self.assertFalse(form.is_valid())
 
     def test_form_field_game_name_does_not_accept_value_empty(self):
-        user = User.objects.get(pk=1)
-        form_data = {"game_name": "", "max_players":20, "planet_name":"dbfgbdfgb"}
+        form_data =  {"planet_name":"fgh", "game_name": "", "game_mode":1, "max_players":20, 
+                     "num_alliances": 3, "bot_def_num": 1,"bot_ofc_num": 2}
         form = gameForm(form_data)
         self.assertFalse(form.is_valid())
 
     def test_form_field_planet_name_does_not_accept_value_empty(self):
-        user = User.objects.get(pk=1)
-        form_data = {"game_name": "ggdf", "max_players":20, "planet_name":""}
+        form_data =  {"planet_name":"", "game_name": "jbh", "game_mode":1, "max_players":20, 
+                     "num_alliances": 3, "bot_def_num": 1,"bot_ofc_num": 2}
         form = gameForm(form_data)
         self.assertFalse(form.is_valid())
