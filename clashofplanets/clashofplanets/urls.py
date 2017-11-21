a72b04d8af2da8453decaf036becc820b426c6a4
Leonardo <leon.mass.music@gmail.com>
2017-11-21 19:28:51 -0300

    reordenamiento de urls

--- clashofplanets/clashofplanets/urls.py
+++ clashofplanets/clashofplanets/urls.py
@@ -15,34 +15,20 @@ urlpatterns = [
     url(r'^signup/$', signup_view, name='signup'),
     url(r'^login/$', Login.as_view(), name='login'),
     url(r'^logout/$', logout, {'next_page': settings.LOGOUT_REDIRECT_URL}),
-    url(r'^game_instructions/$', game_instructions_view,
-        name='game_instructions'),
-    # Game Room List (Lobby):
-    url(r'^game_rooms/$', GameRoomsListView.as_view(), name='game_rooms'),
-    # Make game:
-    url(r'^game_rooms/make_game/$', make_game, name='make_game'),
-    # Makes player and games:
-    url(r'^game_rooms/make_player/$', make_player, name='make_player'),
-    # Actual game room, directed from make_player:
-    url(r'^game_rooms/(?P<game_room_num>[0-9]+)/$', game_room,
-        name='game_room'),
+    url(r'^game_instructions/$', game_instructions_view, name='game_instructions'),
+    url(r'^game_rooms/$', GameRoomsListView.as_view(), name='game_rooms'),  # Game Room List (Lobby)
     url(r'^game_rooms/GameClosed/$', game_closed, name='game_closed'),
-    url(r'^game_rooms/\d+/get_planets/$', send_planets, name='send_planets'),
-    url(r'^game_rooms/\d+/get_game_state/$', send_game_state,
-        name='send_game_state'),
     url(r'^game_rooms/get_games/$', send_games, name='send_games'),
-    url(r'^game_rooms/game/(?P<game_num>[0-9]+)/$', start_game,
-        name='start_game'),
-    url(r'^game_rooms/game/\d+/get_planets/$', send_planets,
-        name='send_planets'),
-    url(r'^game_rooms/game/\d+/change_distribution/$', change_distribution,
-        name='change_distribution'),
-    url(r'^game_rooms/game/\d+/send_attack/$', send_attack,
-        name='send_attack'),
-    url(r'^game_rooms/game/\d+/send_pop/$', send_pop,
-        name='send_pop'),
-    url(r'^game_rooms/game/\d+/missiles_status/$', missiles_status,
-        name='missiles_status'),
-    url(r'^game_rooms/game/(?P<game_num>[0-9]+)/stats/$', game_status,
-        name='game_status')
+    url(r'^game_rooms/make_game/$', make_game, name='make_game'),   # Make game
+    url(r'^game_rooms/make_player/$', make_player, name='make_player'),     # Makes player (Join Game)
+    url(r'^game_rooms/(?P<game_num>[0-9]+)/$', game_room, name='game_room'),   # Actual game room
+    url(r'^game_rooms/(?P<game_num>[0-9]+)/get_planets/$', send_planets, name='send_planets'),
+    url(r'^game_rooms/(?P<game_num>[0-9]+)/get_game_state/$', send_game_state, name='send_game_state'),
+    url(r'^game_rooms/(?P<game_num>[0-9]+)/game/$', start_game, name='start_game'),
+    url(r'^game_rooms/(?P<game_num>[0-9]+)/game/get_planets/$', send_planets, name='send_planets'),
+    url(r'^game_rooms/(?P<game_num>[0-9]+)/game/change_distribution/$', change_distribution, name='change_distribution'),
+    url(r'^game_rooms/(?P<game_num>[0-9]+)/game/send_attack/$', send_attack, name='send_attack'),
+    url(r'^game_rooms/(?P<game_num>[0-9]+)/game/send_pop/$', send_pop, name='send_pop'),
+    url(r'^game_rooms/(?P<game_num>[0-9]+)/game/missiles_status/$', missiles_status, name='missiles_status'),
+    url(r'^game_rooms/(?P<game_num>[0-9]+)/game/stats/$', game_status, name='game_status')
 ]
